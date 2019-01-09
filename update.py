7949420047a5adf0dea8bd5a64ccd08b97233033
Yasuaki Uechi <y@uechi.io>
2019-01-09 14:58:57 +0900

    compatible with Python 3.5

--- update.py
+++ update.py
@@ -12,10 +12,10 @@ def get_ip_address():
     try:
         ip_address = requests.get(endpoint).text
     except Exception:
-        exit(f'{endpoint}: failed')
+        exit('%s failed' % endpoint)
 
     if ip_address == '':
-        exit(f'{endpoint}: failed')
+        exit('%s failed' % endpoint)
 
     if ':' in ip_address:
         ip_address_type = 'AAAA'
@@ -31,7 +31,7 @@ def update_dns(cf, zone_name, zone_id, dns_name, ip_address, ip_address_type):
     try:
         dns_records = cf.zones.dns_records.get(zone_id, params=params)
     except CloudFlare.exceptions.CloudFlareAPIError as e:
-        exit(f'/zones/dns_records {dns_name} - {e} - api call failed')
+        exit('/zones/dns_records %s - %s - api call failed' % (dns_name, e))
     updated = False
 
     for dns_record in dns_records:
@@ -42,11 +42,12 @@ def update_dns(cf, zone_name, zone_id, dns_name, ip_address, ip_address_type):
             continue
 
         if ip_address_type != old_ip_address_type:
-            print(f'ignored: {dns_name} {old_ip_address}; wrong address family')
+            print('ignored: %s %s; wrong address family' % (dns_name,
+                                                            old_ip_address))
             continue
 
         if ip_address == old_ip_address:
-            print(f'unchanged: {dns_name} {ip_address}')
+            print('unchanged: %s %s' % (dns_name, ip_address))
             updated = True
             continue
 
@@ -62,8 +63,9 @@ def update_dns(cf, zone_name, zone_id, dns_name, ip_address, ip_address_type):
             dns_record = cf.zones.dns_records.put(
                 zone_id, dns_record_id, data=dns_record)
         except CloudFlare.exceptions.CloudFlareAPIError as e:
-            exit(f'/zones.dns_records.put {dns_name} - {e} - api call failed')
-        print(f'update: {dns_name} {old_ip_address} -> {ip_address}')
+            exit('/zones.dns_records.put %s %s - api call failed' % (dns_name,
+                                                                     e))
+        print('update: %s %s -> %s' % (dns_name, old_ip_address, ip_address))
         updated = True
 
     if updated:
@@ -78,13 +80,13 @@ def update_dns(cf, zone_name, zone_id, dns_name, ip_address, ip_address_type):
     try:
         dns_record = cf.zones.dns_records.post(zone_id, data=dns_record)
     except CloudFlare.exceptions.CloudFlareAPIError as e:
-        exit(f'/zones.dns_records.post {dns_name} - {e} - api call failed')
-    print(f'CREATED: {dns_name} {ip_address}')
+        exit('/zones.dns_records.post %s %s - api call failed' % (dns_name, e))
+    print('created: %s %s' % (dns_name, ip_address))
 
 
 def update_fqdn(dns_name, ip_address, ip_address_type):
     host_name, zone_name = dns_name.split('.', 1)
-    print(f'pending: {dns_name}')
+    print('pending: %s' % dns_name)
 
     cf = CloudFlare.CloudFlare()
 
@@ -92,15 +94,16 @@ def update_fqdn(dns_name, ip_address, ip_address_type):
         params = {'name': zone_name}
         zones = cf.zones.get(params=params)
     except CloudFlare.exceptions.CloudFlareAPIError as e:
-        exit(f'/zones {e} - api call failed')
+        exit('/zones %s - api call failed' % e)
     except Exception as e:
-        exit(f'/zones.get - {e} - api call failed')
+        exit('/zones.get - %s - api call failed' % e)
 
     if len(zones) == 0:
-        exit(f'/zones.get - {zone_name} - zone not found')
+        exit('/zones.get - %s - zone not found' % zone_name)
 
     if len(zones) != 1:
-        exit(f'/zones.get - {zone_name} - api call returned {len(zones)} items')
+        exit('/zones.get - %s - api call returned {len(zones)} items' %
+             zone_name)
 
     zone = zones[0]
 
@@ -114,13 +117,13 @@ if __name__ == '__main__':
     try:
         dns_list_path = sys.argv[1]
     except IndexError:
-        exit('usage: update.py fqdn-hostname')
+        exit('usage: update.py dnslist.txt')
 
     with open(dns_list_path) as f:
         dns_list = f.read().splitlines()
 
     ip_address, ip_address_type = get_ip_address()
-    print(f'ip: {ip_address}')
+    print('ip: %s' % ip_address)
 
     for dns_name in dns_list:
         update_fqdn(dns_name, ip_address, ip_address_type)
