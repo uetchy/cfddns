88f6542f905e64dc01b47c13bc0dff54b972d671
Yasuaki Uechi <y@uechi.io>
2019-01-09 18:18:57 +0900

    add global option

--- systemd/install.sh
+++ systemd/install.sh
@@ -4,5 +4,5 @@ ROOT_DIR=$(realpath $(dirname $0)/../)
 CONF_DIR=$ROOT_DIR/systemd
 SYSTEM_PATH=/etc/systemd/system
 
-cat $CONF_DIR/cloudflare-ddns.service | sed "s|{{ROOT_DIR}}|$ROOT_DIR|" >$SYSTEM_PATH/cloudflare-ddns.service
+cat $CONF_DIR/cloudflare-ddns.service | sed "s|{{ROOT_DIR}}|$ROOT_DIR|g" >$SYSTEM_PATH/cloudflare-ddns.service
 cp $SCRIPT_DIR/cloudflare-ddns.timer $SYSTEM_PATH/cloudflare-ddns.timer
