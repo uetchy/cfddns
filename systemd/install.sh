8f7ebd3d192ffe7ff11d36b9c1dd492acbc2dcb4
Yasuaki Uechi <y@uechi.io>
2020-03-30 12:08:31 +0900

    fix: script path

--- systemd/install.sh
+++ systemd/install.sh
@@ -5,4 +5,8 @@ CONF_DIR=$ROOT_DIR/systemd
 SYSTEM_PATH=/etc/systemd/system
 
 cat $CONF_DIR/cloudflare-ddns.service | sed "s|{{ROOT_DIR}}|$ROOT_DIR|g" >$SYSTEM_PATH/cloudflare-ddns.service
-cp $SCRIPT_DIR/cloudflare-ddns.timer $SYSTEM_PATH/cloudflare-ddns.timer
+cp $CONF_DIR/cloudflare-ddns.timer $SYSTEM_PATH/cloudflare-ddns.timer
+
+systemctl enable cloudflare-ddns.timer
+systemctl start cloudflare-ddns.timer
+systemctl list-timers --all
